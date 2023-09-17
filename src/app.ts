8ad8bfd6d735ada261a0922c5120678b9f47c06e
kajal-2011 <kajalrekha888@gmail.com>
2023-09-18 02:36:29 +0600

    server setup, types and user model added

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,59 @@
-console.log("hello world!");
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import mongoSenitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import mongoose from "mongoose";
+class App {
+  private app: Application;
+
+  constructor() {
+    this.app = express();
+    this.configureMiddlewares();
+    this.setupRoutes();
+    this.connectToDatabase();
+  }
+
+  private configureMiddlewares(): void {
+    this.app.use(express.json());
+    this.app.use(
+      express.urlencoded({
+        extended: true,
+      }),
+    );
+    this.app.use(cors());
+    this.app.use(morgan("dev"));
+    this.app.use(mongoSenitize());
+    this.app.use(helmet());
+    this.app.use(hpp());
+  }
+
+  private setupRoutes(): void {
+    this.app.get("/", (req: Request, res: Response) => {
+      res.status(200).json({ message: "Welcome to mimosa sarver!" });
+    });
+  }
+
+  private connectToDatabase(): void {
+    const URI = process.env.MONGO_URI as string;
+
+    mongoose
+      .connect(URI)
+      .then(() => {
+        const PORT = process.env.PORT || 4000;
+
+        this.app.listen(PORT, () => {
+          console.log(`Server is running on port: ${PORT}`);
+        });
+      })
+      .catch((error) => {
+        console.log(`MongoDB connection error: ${error}`);
+      });
+  }
+}
+
+dotenv.config();
+
+new App();
---
+++ src/types/beautyPackage.type.ts
@@ -0,0 +1,13 @@
+import { Document } from "mongoose";
+import { bookingType } from "./booking.type";
+import { specialistType } from "./specialist.type";
+
+export type beautyPackageType = {
+  title: string;
+  description: string;
+  category: string;
+  images: string[];
+  price: number;
+  specialists: specialistType[];
+  bookings: bookingType[];
+} & Document;
